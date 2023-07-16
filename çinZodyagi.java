d79656113bc859b1b6ea6834aa34294693720901
ynsemrercan <137263703+ynsemrercan@users.noreply.github.com>
2023-07-16 22:28:16 +0300

    Add files via upload

---
+++ çinZodyagi.java
@@ -0,0 +1,77 @@
+import java.util.Scanner;
+public class çinZodyagi {
+    public static void main(String[] args) {
+        Scanner input=new Scanner(System.in);
+
+        int year;
+
+        System.out.println("Doğum Yılınızı Giriniz: ");
+        year=input.nextInt();
+
+        year%=12;
+
+        switch (year){
+
+            case 0:
+
+                System.out.println(" Maymun ");
+                break;
+
+            case 1:
+
+                System.out.println(" Horoz ");
+                break;
+
+            case 2:
+
+                System.out.println(" Köpek ");
+                break;
+            case 3:
+
+                System.out.println(" Domuz ");
+                break;
+
+            case 4:
+
+                System.out.println(" Fare ");
+                break;
+            case 5:
+
+                System.out.println(" Öküz ");
+                break;
+            case 6:
+
+                System.out.println(" Kaplan ");
+                break;
+
+            case 7:
+
+                System.out.println(" Tavşan ");
+                break;
+
+            case 8:
+
+                System.out.println(" Ejderha ");
+                break;
+
+            case 9:
+
+                System.out.println(" Yılan ");
+                break;
+
+            case 10:
+
+                System.out.println(" At ");
+                break;
+
+            case 11:
+
+                System.out.println(" Koyun ");
+                break;
+
+            default:
+
+                System.out.println("Hatalı Değer Girdiniz ");
+
+        }
+}}
